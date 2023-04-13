c2426c6c9ef85c540f602b9d86eba53143d42478
mfelsper <a63059@ualg.pt>
2023-04-13 15:29:36 +0100

    Add private to atributes in CurrencyApi.

--- CurrencyApi.java
+++ CurrencyApi.java
@@ -1,6 +1,6 @@
 public class CurrencyApi {
-    String uri = "";
-    String apiKey = "8DMyPEUp0j7f1def1nAA4zzhGNphSVl5";
+    private String uri = "";
+    private String apiKey = "8DMyPEUp0j7f1def1nAA4zzhGNphSVl5";
 
 
 }
